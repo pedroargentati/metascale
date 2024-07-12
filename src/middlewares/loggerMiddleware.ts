13b1fe5901761535df41cc0e9d9dc89653e46c47
Pedro <rm88246@fiap.com.br>
2024-07-12 15:13:04 -0300

    feat: add nomiddleware de log  no projeto

---
+++ src/middlewares/loggerMiddleware.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import logger from '../config/logger/logger';
+
+/**
+ * Middleware para logar requisições recebidas.
+ * Registra o método HTTP, URL e código de status da resposta de cada requisição.
+ *
+ * @param {Request} request - O objeto da requisição recebida.
+ * @param {Response} response - O objeto de resposta.
+ * @param {NextFunction} next - A próxima função de middleware.
+ */
+export const logRequest = (request: Request, response: Response, next: NextFunction) => {
+	logger.info(`Requisição :: Método: ${request.method} :: URL: ${request.url} :: Status: ${response.statusCode}`);
+	next();
+};
+
+/**
+ * Middleware para logar erros.
+ * Registra o método HTTP, URL, código de status da resposta e a pilha de erros.
+ * Envia uma resposta de status 500 com uma mensagem de erro genérica.
+ *
+ * @param {Error} error - O objeto de erro.
+ * @param {Request} request - O objeto da requisição recebida.
+ * @param {Response} res - O objeto de resposta.
+ * @param {NextFunction} next - A próxima função de middleware.
+ */
+export const logError = (error: Error, request: Request, response: Response, next: NextFunction) => {
+	logger.error(`[ERRO] :: Método: ${request.method} :: URL: ${request.url} :: Status: ${response.statusCode}`, {
+		stack: error.stack,
+	});
+	response.status(500).send('Algo deu errado!');
+};
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import { getAllCanonico } from './canonico';
+import { logRequest, logError } from '../middlewares/loggerMiddleware';
 
 const router = express.Router();
+
+// Middleware para logar cada requisição
+router.use(logRequest);
 
 router.get('/', (req, res) => {
 	res.send('Bem-vindo à API!');
@@ -9,5 +13,8 @@ router.get('/', (req, res) => {
 
 /** Listar todos canônicos. */
 router.get('/canonicos', getAllCanonico);
+
+// Middleware para tratamento de erros
+router.use(logError);
 
 export default router;
