64404fa4117564fbcd80c4547e5efa66adaa1887
pedroargentati <pedro.argentatii@gmail.com>
2024-08-24 16:55:30 -0300

    feat: index.ts refatorado

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,44 @@ import * as core from 'express-serve-static-core';
 import routes from './routes/routes';
 import consumeAllCanonicos from './app/kafka.app';
 import { INSTANCE_TYPE_API } from './utils/constants';
+import logger from './config/logger/logger';
 
 let app: core.Express | null = null;
 
-if (process.env.INSTANCE_TYPE === INSTANCE_TYPE_API) {
+// Função para inicializar o servidor Express
+function initializeServer() {
 	app = express();
 	app.use(express.json());
 
 	const port = process.env.PORT || 8080;
-
 	app.use('/', routes);
 	setupSwagger(app);
 
 	app.listen(port, () => {
-		console.log(`Servidor está escutando: http://localhost:${port}`);
+		logger.info(`Servidor está escutando: http://localhost:${port}`);
 	});
-} else {
-	(async () => {
-		await consumeAllCanonicos();
-	})();
 }
+
+// Função para inicializar o consumidor Kafka
+async function initializeKafkaConsumer() {
+	try {
+		await consumeAllCanonicos();
+	} catch (error) {
+		logger.error('Erro ao iniciar o consumidor Kafka:', error);
+	}
+}
+
+// Validação da variável de ambiente INSTANCE_TYPE
+if (!process.env.INSTANCE_TYPE) {
+	throw new Error('A variável de ambiente INSTANCE_TYPE não está definida.');
+}
+
+// Inicialização baseada no tipo de instância
+if (process.env.INSTANCE_TYPE === INSTANCE_TYPE_API) {
+	initializeServer();
+} else {
+	initializeKafkaConsumer();
+}
+logger.info(`Inicialização baseada no tipo de instância: ${process.env.INSTANCE_TYPE}`);
 
 export default app;
