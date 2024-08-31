013c96451b34f43570961daf9056b26a9e963f9e
GKazukiOnishi <kazuki.onishi@hotmail.com>
2024-08-31 16:43:53 -0300

    Feat: Implementando autenticação IAM no MSK da AWS

--- src/config/kafka/kafka.config.ts
+++ src/config/kafka/kafka.config.ts
@@ -1,9 +1,33 @@
 import { Kafka, logLevel } from 'kafkajs';
+import { generateAuthToken } from 'aws-msk-iam-sasl-signer-js';
+import { IS_DEV } from '../../utils/constants';
 
-const kafka = new Kafka({
-	clientId: process.env.KAFKA_CLIENT_ID,
-	brokers: process.env.KAFKA_BROKERS!?.split(',') || ['localhost:9092'],
-	logLevel: logLevel.INFO,
-});
+async function oauthBearerTokenProvider(region: string) {
+	const authTokenResponse = await generateAuthToken({ region });
+	return {
+		value: authTokenResponse.token,
+	};
+}
+
+let kafka: Kafka;
+
+if (!IS_DEV) {
+	kafka = new Kafka({
+		clientId: process.env.KAFKA_CLIENT_ID,
+		brokers: process.env.KAFKA_BROKERS!.split(','),
+		logLevel: logLevel.INFO,
+		ssl: true,
+		sasl: {
+			mechanism: 'oauthbearer',
+			oauthBearerProvider: () => oauthBearerTokenProvider(process.env.AWS_REGION!),
+		},
+	});
+} else {
+	kafka = new Kafka({
+		clientId: process.env.KAFKA_CLIENT_ID,
+		brokers: process.env.KAFKA_BROKERS!.split(','),
+		logLevel: logLevel.INFO,
+	});
+}
 
 export default kafka;
