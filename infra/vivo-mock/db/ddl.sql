a538441b2af96b3a58fa9647c74b774bfe1f8b5e
Pedro <rm88246@fiap.com.br>
2024-09-06 23:58:30 -0300

    chore: novo ddl para mock test

--- infra/vivo-mock/db/ddl.sql
+++ infra/vivo-mock/db/ddl.sql
@@ -1,33 +1,37 @@
 CREATE DATABASE IF NOT EXISTS VivoTest;
 
-USE VivoTest;
+USE metascale_database;
 
 -- Tabela de Produtos/Serviços
-CREATE TABLE IF NOT EXISTS Produtos (
-    produto_id INT AUTO_INCREMENT PRIMARY KEY,
-    nome_produto VARCHAR(100) NOT NULL,
-    descricao TEXT,
-    data_lancamento DATE,
-    status VARCHAR(50) DEFAULT 'Em Teste'
+CREATE TABLE product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    description TEXT,
+    release_date DATE,
+    product_type ENUM('PHYSICAL', 'DIGITAL') NOT NULL  -- Enum para o tipo de produto
 );
+
 
 -- Tabela de Clientes
-CREATE TABLE IF NOT EXISTS Clientes (
-    cliente_id INT AUTO_INCREMENT PRIMARY KEY,
-    nome_cliente VARCHAR(100) NOT NULL,
-    email_cliente VARCHAR(100) UNIQUE NOT NULL,
-    telefone_cliente VARCHAR(20),
-    cidade VARCHAR(50),
-    estado VARCHAR(50)
+CREATE TABLE client (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(255) NOT NULL,
+    customer_email VARCHAR(255),
+    customer_phone VARCHAR(20),
+    city VARCHAR(100),
+    state VARCHAR(50)
 );
 
+
 -- Tabela de Associação entre Clientes e Produtos (Relacionamento)
-CREATE TABLE IF NOT EXISTS ClienteProduto (
-    cliente_produto_id INT AUTO_INCREMENT PRIMARY KEY,
-    cliente_id INT,
-    produto_id INT,
-    data_associacao DATE NOT NULL,
-    feedback TEXT,
-    FOREIGN KEY (cliente_id) REFERENCES Clientes(cliente_id),
-    FOREIGN KEY (produto_id) REFERENCES Produtos(produto_id)
+CREATE TABLE customerproduct (
+    customer_product_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    product_id INT NOT NULL,
+    association_date DATE,
+    feedback VARCHAR(255),
+    status ENUM('ACTIVE', 'INACTIVE') NOT NULL,  -- Enum para o status
+
+    FOREIGN KEY (customer_id) REFERENCES client(customer_id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
 );
